c11f6a5d68516a070dbd515e7f884b1813e4493c
Yan Luo <yanluo@andrew.cmu.edu>
2023-10-26 19:49:42 -0400

    updated dump_raw_counts.py

--- scripts/dump_raw_counts.py
+++ scripts/dump_raw_counts.py
@@ -1,40 +1,23 @@
-import pandas as pd
-import pyarrow.parquet as pq
+import duckdb
 
-import pyarrow.parquet as pq
+try:
+    with duckdb.connect('main.db') as conn:
+        tables = [
+            "bike_data",
+            "central_park_weather",
+            "fhv_bases",
+            "fhv_tripdata",
+            "fhvhv_tripdata",
+            "green_tripdata",
+            "yellow_tripdata"
+        ]
 
-def count_rows_in_csv(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        row_count = sum(1 for line in f)
-    return row_count - 1  # subtract 1 to exclude the header
-
-def count_rows_in_parquet(file_path):
-    parquet_file = pq.ParquetFile(file_path)
-    return parquet_file.metadata.num_rows
-
-# Paths to your CSV files
-csv_files = [
-    '/home/yanluo/cmu-95797-data-warehousing/data/citibike-tripdata.csv/citibike-tripdata.csv',
-    '/home/yanluo/cmu-95797-data-warehousing/data/central_park_weather.csv',
-    '/home/yanluo/cmu-95797-data-warehousing/data/fhv_bases.csv'
-]
-file_names = ['bike_data', 'central_park_weather', 'fhv_bases']
-
-# Count rows in CSV files
-for csv_file, file_name in zip(csv_files, file_names):
-    row_count_csv = count_rows_in_csv(csv_file)
-    print(f"Row count in {file_name}: {row_count_csv}")
-
-# Paths to your Parquet files
-parquet_files = [
-    '/home/yanluo/cmu-95797-data-warehousing/data/taxi/fhv_tripdata.parquet',
-    '/home/yanluo/cmu-95797-data-warehousing/data/taxi/fhvhv_tripdata.parquet',
-    '/home/yanluo/cmu-95797-data-warehousing/data/taxi/green_tripdata.parquet',
-    '/home/yanluo/cmu-95797-data-warehousing/data/taxi/yellow_tripdata.parquet'
-]
-table_names = ['fhv_tripdata', 'fhvhv_tripdata', 'green_tripdata', 'yellow_tripdata']
-
-# Count rows in Parquet files
-for parquet_file, table_name in zip(parquet_files, table_names):
-    row_count_parquet = count_rows_in_parquet(parquet_file)
-    print(f"Row count in {table_name}: {row_count_parquet}")
+        for table in tables:
+            try:
+                query = f"SELECT COUNT(*) FROM {table}"
+                row_count = conn.execute(query).fetchone()[0]
+                print(f"{table}: {row_count}")
+            except Exception as e:
+                print(f"Failed to fetch row count for {table}: {str(e)}")
+except Exception as e:
+    print(f"Failed to connect to the database: {str(e)}")
--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -1,27 +1,30 @@
-SELECT 'This is the schema for bike_data table:';
+.echo on
+
+-- Schema for bike_data table
 DESCRIBE bike_data;
 SELECT '';
 
-SELECT 'This is the schema for central_park_weather table:';
+-- Schema for central_park_weather table
 DESCRIBE central_park_weather;
 SELECT '';
 
-SELECT 'This is the schema for bike_data table:';
+-- Schema for fhv_bases table
 DESCRIBE fhv_bases;
 SELECT '';
 
-SELECT 'This is the schema for fhv_tripdata table:';
+-- Schema for fhv_tripdata table
 DESCRIBE fhv_tripdata;
 SELECT '';
 
-SELECT 'This is the schema for fhvhv_tripdata table:';
+-- Schema for fhvhv_tripdata table
 DESCRIBE fhvhv_tripdata;
 SELECT '';
 
-SELECT 'This is the schema for green_tripdata table:';
+-- Schema for green_tripdata table
 DESCRIBE green_tripdata;
 SELECT '';
 
-SELECT 'This is the schema for yellow_tripdata table:';
+-- Schema for yellow_tripdata table
 DESCRIBE yellow_tripdata;
 SELECT '';
+
