f9349a1450e468724c6d017579a66d4e254dc33b
Yan Luo <yanluo@andrew.cmu.edu>
2023-11-03 12:55:27 -0400

    updated stg__bike_data model

--- nyc_transit/models/staging/stg__bike_data.sql
+++ nyc_transit/models/staging/stg__bike_data.sql
@@ -8,41 +8,33 @@ renamed as (
     
     select
         tripduration::int as tripduration,
-        starttime::timestamp as starttime,
-        stoptime::timestamp as stoptime,
-        "start station id"::int as "start station id",
-        "start station name",
-        "start station latitude"::double as "start station latitude",
-        "start station longitude"::double as "start station longitude",
-        "end station id"::int as "end station id",
-        "end station name",
-        "end station latitude"::double as "end station latitude",
-        "end station longitude"::double as "end station longitude",
+        COALESCE(started_at::timestamp, starttime::timestamp) AS started_at,
+        COALESCE(ended_at::timestamp, stoptime::timestamp) AS ended_at,
+        COALESCE(start_station_id::double, "start station id"::double) AS start_station_id,
+        COALESCE(end_station_id::double, "end station id"::double) AS end_station_id,
+        COALESCE(start_station_name, "start station name") AS start_station_name,
+        COALESCE(end_station_name, "end station name") AS end_station_name,
+        COALESCE(start_lat::double, "start station latitude"::double) AS start_lat,
+        COALESCE(start_lng::double, "start station longitude"::double) AS start_lng,
+        COALESCE(end_lat::double, "end station latitude"::double) AS end_lat,
+        COALESCE(end_lng::double, "end station longitude"::double) AS end_lng,
         bikeid::int as bikeid,
-        usertype,
-        "birth year"::int as "birth year",
+        "birth year"::int as birth_year,
         {{ convert_gender('gender') }} as gender,
         ride_id,
         rideable_type,
-        started_at::timestamp as started_at,
-        ended_at::timestamp as ended_at,
-        start_station_name,
-        start_station_id::double as start_station_id,
-        end_station_name,
-        end_station_id::double as end_station_id,
-        start_lat::double as start_lat,
-        start_lng::double as start_lng,
-        end_lat::double as end_lat,
-        end_lng::double as end_lng,
-        member_casual,
+        COALESCE(member_casual, 
+            CASE 
+                WHEN usertype = 'Subscriber' THEN 'member' 
+                WHEN usertype = 'Customer' THEN 'casual' 
+                ELSE usertype 
+            END) as member_type,
         filename
 
     from source
 
     where 
         tripduration > 0 AND 
-        starttime <= '2022-12-31' AND 
-        stoptime <= '2022-12-31' AND 
         started_at <= '2022-12-31' AND 
         ended_at <= '2022-12-31'
 )
